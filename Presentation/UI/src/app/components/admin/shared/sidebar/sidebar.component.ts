75ee173d2df5d83607cb1db196f8b7998fb6b4d1
Narhuz.fasouy321 <35305@students.riphah.edu.pk>
2025-10-09 19:03:03 +0500

    feat: add stores component with styling, data loading, filtering, and modal functionality

--- Presentation/UI/src/app/components/admin/shared/sidebar/sidebar.component.ts
+++ Presentation/UI/src/app/components/admin/shared/sidebar/sidebar.component.ts
@@ -23,6 +23,7 @@ export class SidebarComponent {
     { icon: 'people', label: 'User Management', route: '/admin/users' },
     { icon: 'work', label: 'Project Management', route: '/admin/project-management' },
     { icon: 'precision_manufacturing', label: 'Machine Inventory', route: '/admin/machine-inventory' },
-    { icon: 'assignment', label: 'Machine Assignments', route: '/admin/machine-assignments' }
+    { icon: 'assignment', label: 'Machine Assignments', route: '/admin/machine-assignments' },
+    { icon: 'store', label: 'Store Management', route: '/admin/stores' }
   ];
 }
